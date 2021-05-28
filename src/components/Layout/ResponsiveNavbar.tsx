704c7e8477197d289b6052870434f15f3e745231
Rimarok <romaric.ruga@gmail.com>
2021-05-28 08:40:09 +0200

    btn-outline + ResponsiveNavbar order & outline

--- src/components/Layout/ResponsiveNavbar.tsx
+++ src/components/Layout/ResponsiveNavbar.tsx
@@ -2,13 +2,13 @@ import React from "react"
 
 export default function ResponsiveNavbar({ pageId }) {
   return (
-    <nav className="navbar navbar-expand-lg position-absolute">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg position-absolute w-100">
+      <div className="container-fluid justify-content-between">
         <a className="navbar-brand" href="/">
           <img src="/highbs-bok-letters.svg" />
         </a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler btn btn-purple mx-2 px-3 rounded-pill shadow"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbar"
@@ -16,7 +16,7 @@ export default function ResponsiveNavbar({ pageId }) {
           aria-expanded="false"
           aria-label="Déplier le menu"
         >
-          <span className="navbar-toggler-icon"></span>
+          Menu
         </button>
         <div className="collapse navbar-collapse" id="navbar">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -24,26 +24,30 @@ export default function ResponsiveNavbar({ pageId }) {
               <a
                 className={`nav-link ${
                   pageId === "l-univers/index" && "active"
-                }`}
+                } btn btn-outline-purple mx-2 px-3 rounded-pill shadow`}
                 href="/l-univers"
               >
-                Découvrir l'univers
+                L'univers
               </a>
             </li>
             <li className="nav-item">
               <a
-                className={`nav-link ${pageId === "acheter" && "active"}`}
-                href="/acheter"
-              >
-                Acheter le livret
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className={`nav-link ${pageId === "a-propos" && "active"}`}
+                className={`nav-link ${
+                  pageId === "a-propos" && "active"
+                } btn btn-outline-purple mx-2 px-3 rounded-pill shadow`}
                 href="/a-propos"
               >
                 À propos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className={`nav-link ${
+                  pageId === "acheter" && "active"
+                } btn btn-purple mx-2 px-3 rounded-pill shadow`}
+                href="/acheter"
+              >
+                Acheter
               </a>
             </li>
           </ul>
