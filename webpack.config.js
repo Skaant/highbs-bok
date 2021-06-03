ef9949a894d10a188d51e774a33803591bf3f744
Rimarok <romaric.ruga@gmail.com>
2021-06-03 05:55:55 +0200

    workflows: fix webpack output folder to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devtool: "eval-source-map",
   entry: resolve(__dirname, "src", "scripts", "script.js"),
   output: {
-    path: resolve(__dirname, "static"),
+    path: resolve(__dirname, "public"),
     filename: "script.js",
   },
   resolve: {
