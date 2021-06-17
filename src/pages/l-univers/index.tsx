6c6d59678dba3c76d212e0c2803ee8aee38eadea
Rimarok <romaric.ruga@gmail.com>
2021-06-17 21:07:19 +0200

    l-univers: group sub-pages as btn-menu + cta achat

--- src/pages/l-univers/index.tsx
+++ src/pages/l-univers/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Header } from "../../components/Header"
 import { Layout } from "../../components/Layout/Layout"
+import { EXTRACT_DOWNLOAD_LINK } from "../../config"
 import "../../styles/global.scss"
 
 const title = "Découvrir l'univers"
@@ -13,7 +14,7 @@ export default function () {
         <div className="container-fluid px-0">
           <div
             className="position-absolute w-100 full-img-container align-items-center"
-            style={{ height: "max-content", marginTop: "calc(10vh - 200px)" }}
+            style={{ height: "max-content", marginTop: "calc(100px - 10vh)" }}
           >
             <img
               src="/images/hb1-soln-syst.svg"
@@ -21,34 +22,59 @@ export default function () {
               data-parallax-direction="up"
             />
           </div>
-          <div className="row px-4 pt-4 pb-5">
+          <div className="row px-4 py-4">
             <div className="col-12 col-md-10 col-lg-8 col-xl-6">
-              <h1>{title}</h1>
-              <p>
-                Que de péripéties ont vécu les ZUMS avant d'en arriver jusque là
-                ...
+              <h1 className="mb-5">{title}</h1>
+              <p className="mb-5">
+                Le <span className="bg-gold">tout-un</span> ... que de{" "}
+                <b>péripéties</b> ont vécu les ZUMS avant d'en arriver jusque
+                là.
               </p>
               <p>
                 Imaginez un peu :{" "}
                 <b>partis de leur planète TERA à bord d'une forêt spatiale</b>,
                 ces créatures proprement fabuleuses{" "}
                 <b>
-                  ont été ensuite dispersés jusqu'aux frontières de l'univers
+                  ont été ensuite dispersées jusqu'aux frontières de l'univers
                 </b>
                 .
               </p>
-              <p>
-                Et ce n'est pas tout ! Ils en sont revenus pour former l'ANGK :
-                l'être unique capable de conceptualiser la pensée libératrice,
-                le concept ultime : l'HIGHBS.
+              <p className="mb-5">
+                Et ce n'est pas tout !{" "}
+                <b>Ils en sont revenus pour former l'ANGK</b> : l'être unique{" "}
+                <b>capable de conceptualiser la pensée libératrice</b>, le
+                concept ultime : <b>l'HIGHBS</b>.
               </p>
               <p>
-                Servez-vous de ces quelques pages pour explorer plus encore
-                l'univers épique des ZUMS.
+                Servez-vous de ces quelques pages pour{" "}
+                <b>explorer plus encore l'univers épique des ZUMS</b>.
               </p>
+              <div className="menu-btn mt-5">
+                <a href="/l-univers/sept-eras" className="btn btn-purple">
+                  Sept ERAS
+                </a>
+                <a href="/l-univers/sept-eras" className="btn btn-purple">
+                  Les couleurs
+                </a>
+                <a href="/l-univers/glossaire" className="btn btn-purple">
+                  Glossaire
+                </a>
+                <a
+                  className="btn btn-outline-deep disabled"
+                  title="Cette page n'est pas encore prête !"
+                >
+                  Personnages et entités
+                </a>
+                <a
+                  className="btn btn-outline-deep disabled"
+                  title="Cette page n'est pas encore prête !"
+                >
+                  Lieux
+                </a>
+              </div>
             </div>
           </div>
-          <div className="row px-4 pt-4 pb-5">
+          <div className="row px-4 pt-4">
             <div className="col-12 col-xl-10">
               <p className="display-4 text-muted">
                 "Lorsqu’il ne restera plus dans cet univers qu’un monde,
@@ -64,38 +90,20 @@ export default function () {
               </p>
             </div>
           </div>
-          <div className="row px-4 pt-5 justify-content-start">
-            <div className="col-12 col-md-10 col-lg-8 col-xl-6 offset-xl-2">
-              <h2>Les sept périodes de l'histoire des ZUMS</h2>
-              <div>
-                <a
-                  href="/l-univers/sept-eras"
-                  className="btn rounded-pill btn-purple"
-                >
-                  Sept ERAS
+          <div className="row px-4 bg-gold">
+            <div className="col-12 col-md-10 col-lg-8 col-xl-6">
+              <h2>Autres pages du site</h2>
+              <div className="menu-btn">
+                <a href="/a-propos" className="btn btn-outline-purple">
+                  À propos
+                </a>
+                <a href={EXTRACT_DOWNLOAD_LINK} className="btn btn-light">
+                  Télécharger l'extrait
+                </a>
+                <a href="/acheter" className="btn btn-purple">
+                  Acheter le livret
                 </a>
               </div>
-            </div>
-          </div>
-          <div className="row px-4 pt-5 justify-content-start">
-            <div className="col-12 col-md-10 col-lg-8 col-xl-6 offset-md-2 offset-lg-4">
-              <h2>Concepts et pages de la section univers</h2>
-              <ul>
-                <li>
-                  <a href="/l-univers/sept-eras">Sept ERAS</a>, ,
-                </li>
-                <li>
-                  <a href="/l-univers/glossaire">Le glossaire</a> des termes
-                  spécifiques à l' HIGHBS-BOK,
-                </li>
-                <li>
-                  <a href="/l-univers/couleurs">Les couleurs</a> utilisées dans
-                  les illustrations.
-                </li>
-                <li>
-                  <a className="text-muted"></a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
