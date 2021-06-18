b756200fa8bbe2f67da4bb42cdc00821b68ed345
Rimarok <romaric.ruga@gmail.com>
2021-06-18 15:12:26 +0200

    Footer.tsx: extract from Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { ReactElement } from "react"
+import { Footer } from "./Footer"
 import ResponsiveNavbar from "./ResponsiveNavbar"
 
 export function Layout({
@@ -14,69 +15,7 @@ export function Layout({
       <div id="layout">
         <ResponsiveNavbar pageId={pageId} />
         {children}
-        <footer className="container-fluid bg-deep">
-          <div className="row py-5">
-            <div className="col-12 col-md-6">
-              <div className="mx-auto px-5 py-3" style={{ maxWidth: "400px" }}>
-                <ul className="list-unstyled">
-                  <li>
-                    <a href="/">Accueil</a>
-                  </li>
-                  <li>
-                    <a href="/l-univers">Découvrir l'univers</a>
-                    <ul className="list-unstyled ps-4 py-3">
-                      <li>
-                        <a href="/l-univers/sept-eras">
-                          Les sept ERAS de l'histoire des ZUMS
-                        </a>
-                      </li>
-                      <li>
-                        <a href="/l-univers/glossaire">Glossaire</a>
-                      </li>
-                      <li>
-                        <a href="/l-univers/couleurs">
-                          Signification des couleurs
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="/acheter">Acheter le livret</a>
-                  </li>
-                  <li>
-                    <a href="/a-propos">À propos</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-12 col-md-6 col-xl-4">
-              <div className="mx-auto px-5 py-3" style={{ maxWidth: "400px" }}>
-                <p className="h4">HIGHBS-BOK 2021</p>
-                <p>
-                  Livret et site par Romaric Ruga (aka.{" "}
-                  <a href="https://imrok.fr">IMROK</a>
-                  <i>, l'artiste</i>) (aka.{" "}
-                  <a href="https://rimarok.com">RIMAROK</a>
-                  <i>, le développeur</i>).
-                </p>
-                <div>
-                  <button
-                    id="footer__contact"
-                    className="btn btn-light rounded-pill my-2 mx-sm-2 px-3"
-                  >
-                    Contact
-                  </button>
-                  <a
-                    href="/mentions-legales"
-                    className="btn btn-outline-light rounded-pill bg-deep my-2 mx-sm-2 px-3"
-                  >
-                    Mentions légales
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </>
   )
