5133d1c1ceb57de6172043dbea99e32839ea7aca
Rimarok <romaric.ruga@gmail.com>
2022-04-27 17:54:14 +0200

    TableOfContent: unstyled props, default true

--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -2,11 +2,13 @@ import React from "react"
 
 export function TableOfContent({
   sections,
+  unstyled = true,
 }: {
   sections: { [key: string]: string }
+  unstyled?: boolean
 }) {
   return (
-    <ul className="list-unstyled">
+    <ul className={`${unstyled ? "list-unstyled" : ""}`}>
       {Object.entries(sections).map(([id, label]) => (
         <li key={id}>
           <a href={"#" + id}>{label}</a>
