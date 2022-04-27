bc78fafdad21955772ee7663c11900758dfcadbb
Rimarok <romaric.ruga@gmail.com>
2022-04-27 17:56:10 +0200

    ImageRowPositionAbsolute: className prop

--- src/components/_rows/ImageRowPositionAbsolute.tsx
+++ src/components/_rows/ImageRowPositionAbsolute.tsx
@@ -3,21 +3,20 @@ import React, { CSSProperties, ReactElement } from "react"
 export function ImageRowPositionAbsolute({
   src,
   style,
+  className,
   imgClassName,
 }: {
   src: string
   style?: CSSProperties
+  className?: string
   imgClassName?: string
 }) {
   return (
     <div
-      className="position-absolute w-100 full-img-container align-items-center"
+      className={`position-absolute w-100 full-img-container align-items-center ${className}`}
       style={style}
     >
-      <img
-        src={src}
-        className={`w-100 cover position-absolute ${imgClassName || ""}`}
-      />
+      <img src={src} className={`w-100 cover ${imgClassName || ""}`} />
     </div>
   )
 }
