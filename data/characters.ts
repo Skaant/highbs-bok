6c2129c69f2df4eb7af866a3ed10a1829a8debbb
Rimarok <romaric.ruga@gmail.com>
2022-04-29 20:07:49 +0200

    characters data: add eras, summary & color props

--- data/characters.ts
+++ data/characters.ts
@@ -1,15 +1,52 @@
+import { colors } from "./colors"
+import { ERAS } from "./eras"
+
 export enum CHARACTERS {
   TOUT_UN = "TOUT_UN",
   BALTAZUM = "BALTAZUM",
   ANGK = "ANGK",
+  GRAND_ARBRE = "GRAND_ARBRE",
 }
 
-type CharacterData = {
+export type CharacterData = {
   name: string
+  eras: ERAS[] | "all"
+  summary: string
+  color: colors
 }
 
 export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
-  TOUT_UN: { name: "Tout-un" },
-  BALTAZUM: { name: "Baltazum" },
-  ANGK: { name: "ANGK" },
+  TOUT_UN: {
+    name: "Le tout-un",
+    eras: "all",
+    summary:
+      "L'entité complète, en potentiel, que les ZUMS cherchent à manifester.",
+    color: colors.gold,
+  },
+  BALTAZUM: {
+    name: "Baltazum",
+    eras: [ERAS.CONTRACTION_ERA],
+    summary:
+      "Voyant son monde sombrer dans les ténèbres, il cherche de nouvelles idées pour éviter qu'il ne disparaisse.",
+    color: colors.skin,
+  },
+  ANGK: {
+    name: "L'ANGK",
+    eras: [ERAS.AURO_ERA, ERAS.ANGK_ERA],
+    summary:
+      "Le projet de corps pour l'être unique capable de réaliser l'HIGHBS et former le tout-un.",
+    color: colors.darkSkin,
+  },
+  GRAND_ARBRE: {
+    name: "Grand-Arbre",
+    eras: [
+      ERAS.PRIMA_ERA,
+      ERAS.EXPANSION_ERA,
+      ERAS.DISPERSION_ERA,
+      ERAS.CONTRACTION_ERA,
+    ],
+    summary:
+      "La structure organique qui sert de support à la colonisation de l'univers par les ZUMS.",
+    color: colors.green,
+  },
 }
