005802bded9c90925c93ab5e1a4b445089430447
Rimarok <romaric.ruga@gmail.com>
2022-04-29 21:10:41 +0200

    characters data: add VOLUMES

--- data/characters.ts
+++ data/characters.ts
@@ -1,5 +1,6 @@
 import { colors } from "./colors"
 import { ERAS } from "./eras"
+import { VOLUMES } from "./volumes"
 
 export enum CHARACTERS {
   TOUT_UN = "TOUT_UN",
@@ -13,15 +14,18 @@ export type CharacterData = {
   eras: ERAS[] | "all"
   summary: string
   color: colors
+  /** First volume when the character appears. */
+  volume: VOLUMES
 }
 
 export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
   TOUT_UN: {
-    name: "Le tout-un",
+    name: "Le Tout-Un",
     eras: "all",
     summary:
       "L'entité complète, en potentiel, que les ZUMS cherchent à manifester.",
     color: colors.gold,
+    volume: VOLUMES.VOLUME_1,
   },
   BALTAZUM: {
     name: "Baltazum",
@@ -29,13 +33,15 @@ export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
     summary:
       "Voyant son monde sombrer dans les ténèbres, il cherche de nouvelles idées pour éviter qu'il ne disparaisse.",
     color: colors.skin,
+    volume: VOLUMES.VOLUME_1,
   },
   ANGK: {
-    name: "L'ANGK",
+    name: "L'Angk",
     eras: [ERAS.AURO_ERA, ERAS.ANGK_ERA],
     summary:
       "Le projet de corps pour l'être unique capable de réaliser l'HIGHBS et former le tout-un.",
     color: colors.darkSkin,
+    volume: VOLUMES.VOLUME_1,
   },
   GRAND_ARBRE: {
     name: "Grand-Arbre",
@@ -48,5 +54,6 @@ export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
     summary:
       "La structure organique qui sert de support à la colonisation de l'univers par les ZUMS.",
     color: colors.green,
+    volume: VOLUMES.VOLUME_1,
   },
 }
