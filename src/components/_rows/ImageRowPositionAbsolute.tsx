9c2e839280adfac01ec1746ca42ed1827b419c8a
Rimarok <romaric.ruga@gmail.com>
2022-04-29 22:37:01 +0200

    ImageRowPositionAbsolute: add imgStyle prop

--- src/components/_rows/ImageRowPositionAbsolute.tsx
+++ src/components/_rows/ImageRowPositionAbsolute.tsx
@@ -3,12 +3,14 @@ import React, { CSSProperties, ReactElement } from "react"
 export function ImageRowPositionAbsolute({
   src,
   style,
+  imgStyle,
   className,
   imgClassName,
 }: {
   src: string
   style?: CSSProperties
   className?: string
+  imgStyle?: CSSProperties
   imgClassName?: string
 }) {
   return (
@@ -16,7 +18,11 @@ export function ImageRowPositionAbsolute({
       className={`position-absolute w-100 full-img-container align-items-center ${className}`}
       style={style}
     >
-      <img src={src} className={`w-100 cover ${imgClassName || ""}`} />
+      <img
+        src={src}
+        className={`w-100 cover ${imgClassName || ""}`}
+        style={imgStyle}
+      />
     </div>
   )
 }
