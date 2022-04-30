579a8825fcf82e77b53f12430cb7069184c1b05e
Rimarok <romaric.ruga@gmail.com>
2022-04-30 11:20:59 +0200

    characters data: add vol 2 characters

--- data/characters.ts
+++ data/characters.ts
@@ -7,6 +7,18 @@ export enum CHARACTERS {
   BALTAZUM = "BALTAZUM",
   ANGK = "ANGK",
   GRAND_ARBRE = "GRAND_ARBRE",
+  SIEGE_PENSEE = "SIEGE_PENSEE",
+  RAZ_LA = "RAZ_LA",
+  BINDA = "BINDA",
+  ZA_ZOZ = "ZA_ZOZ",
+  KRAO = "KRAO",
+  JING_JAM_ZUM = "JING_JAM_ZUM",
+  ENTROPIUS = "ENTROPIUS",
+  /** 
+                <li>
+                  Les trois anciens-anciens qui guident Binda (PALÉO-ERA),
+                </li>
+                <li>Quelques ZUMS de la cité de l'aube (AURO-ERA).</li> */
 }
 
 export type CharacterData = {
@@ -16,6 +28,7 @@ export type CharacterData = {
   color: COLORS
   /** First volume when the character appears. */
   volume: VOLUMES
+  group?: boolean
 }
 
 export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
@@ -47,7 +60,7 @@ export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
     name: "Grand-Arbre",
     eras: [
       ERAS.PRIMA_ERA,
-      ERAS.EXPANSION_ERA,
+      ERAS.EXPANS_ERA,
       ERAS.DISPERSION_ERA,
       ERAS.CONTRACTION_ERA,
     ],
@@ -55,5 +68,57 @@ export const CHARACTERS_DATA: { [key in CHARACTERS]: CharacterData } = {
       "La structure organique qui sert de support à la colonisation de l'univers par les ZUMS.",
     color: COLORS.GREEN,
     volume: VOLUMES.VOLUME_1,
+  },
+  SIEGE_PENSEE: {
+    name: "Le Siège de la Pensée",
+    eras: [ERAS.CONTRACTION_ERA, ERAS.AURO_ERA],
+    summary:
+      "Le point de convergence de tous les idées de l'univers, le cerveau ultime.",
+    color: COLORS.GOLD,
+    volume: VOLUMES.VOLUME_2,
+  },
+  RAZ_LA: {
+    name: "Raz-la-pouëpouët",
+    eras: [ERAS.CONTRACTION_ERA],
+    summary: "Le KAMI du jardin de Baltazum",
+    color: COLORS.TURQUOIZE,
+    volume: VOLUMES.VOLUME_2,
+  },
+  BINDA: {
+    name: "Binda",
+    eras: [ERAS.PALEO_ERA],
+    summary:
+      "Un membre de la tribu PSIK qui va participer à l'émergence du HOL-ONG.",
+    color: COLORS.BLUE,
+    volume: VOLUMES.VOLUME_2,
+  },
+  ZA_ZOZ: {
+    name: "Za-Zoz",
+    eras: [ERAS.PALEO_ERA, ERAS.CONTRACTION_ERA],
+    summary: "KAMI de la tribu PSIK, interlocuteur de Binda et ses amis.",
+    color: COLORS.PURPLE,
+    volume: VOLUMES.VOLUME_2,
+  },
+  KRAO: {
+    name: "Krao",
+    eras: [ERAS.EXPANS_ERA],
+    summary:
+      "Membre de la tribu KOLOS, candidat à l'exil pour coloniser un territoire vacant.",
+    color: COLORS.LIGHT_BLUE,
+    volume: VOLUMES.VOLUME_2,
+  },
+  JING_JAM_ZUM: {
+    name: "Jing-Jam-Zum",
+    eras: [ERAS.AURO_ERA],
+    summary: "Hérault de l'ombre et meneuse du siège de la Cité de l'Aube.",
+    color: COLORS.LIGHT_DEEP,
+    volume: VOLUMES.VOLUME_2,
+  },
+  ENTROPIUS: {
+    name: "Entropius",
+    eras: [ERAS.AURO_ERA],
+    summary: "KAMI de l'entropie, allié de Jing-Jam-Zum et de sa harde.",
+    color: COLORS.DEEP,
+    volume: VOLUMES.VOLUME_2,
   },
 }
