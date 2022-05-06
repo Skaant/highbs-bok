aa496fb40b9aa4995e38a7281be93aff722e2d89
Rimarok <romaric.ruga@gmail.com>
2022-05-06 08:10:55 +0200

    Term tsx: add pluralSign prop & use regular

--- src/components/Term.tsx
+++ src/components/Term.tsx
@@ -6,39 +6,52 @@ import { TermData, TERMS, TERMS_DATA } from "../../data/terms"
 function Term({
   term,
   plural = false,
+  pluralSign = "S",
   link = true,
   outline = false,
+  textColor: _textColor,
   className: _className,
 }: {
-  term: TERMS | Pick<TermData, "name" | "short" | "summary" | "color">
+  term:
+    | TERMS
+    | Pick<TermData, "name" | "short" | "summary" | "color" | "regular">
   plural?: boolean
+  pluralSign?: string
   link?: boolean | string
   outline?: boolean
+  textColor?: COLORS
   className?: string
 }) {
-  const { name, short, summary, color } =
+  const { name, short, summary, color, regular } =
     typeof term === "object" ? term : TERMS_DATA[term]
-  const textColor = COLORS_DATA[color]?.colorWhite ? "light" : "dark"
+  const textColor = _textColor
+    ? _textColor.toLowerCase()
+    : COLORS_DATA[color]?.colorWhite
+    ? "light"
+    : "dark"
   const className = `term bg-${color.toLowerCase()} text-${textColor} ${
     outline ? `border border-2 border-${textColor}` : ""
-  } ${_className || ""}`
+  } ${regular ? "font-regular" : ""} ${_className || ""}`
   return link ? (
     <a
       href={
         typeof link === "string"
           ? link
-          : `${PAGES_DATA[PAGES.L_UNIVERS_GLOSSAIRE].url}#${name}`
+          : `${PAGES_DATA[PAGES.L_UNIVERS_GLOSSAIRE].url}#${name.replace(
+              / /g,
+              "-"
+            )}`
       }
       className={className}
       title={`${short}.\n${summary}`}
     >
       {name}
-      {plural ? "S" : ""}
+      {plural ? pluralSign : ""}
     </a>
   ) : (
     <span className={className} title={`${short}.\n${summary}`}>
       {name}
-      {plural ? "S" : ""}
+      {plural ? pluralSign : ""}
     </span>
   )
 }
