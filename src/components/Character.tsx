75c34598cb4b0e75027575fd9a0a6fe1ddeb02cd
Rimarok <romaric.ruga@gmail.com>
2022-05-06 08:09:23 +0200

    Character tsx: add outline prop

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,21 +1,23 @@
 import * as React from "react"
 import { CHARACTERS, CHARACTERS_DATA } from "../../data/characters"
-import { ERAS, ERAS_DATA } from "../../data/eras"
+import { ERAS_DATA } from "../../data/eras"
 import { PAGES, PAGES_DATA } from "../../data/pages"
 import Term from "./Term"
 
 function Character({
   character,
+  outline,
   className,
 }: {
   character: CHARACTERS
+  outline?: boolean
   className?: string
 }) {
   const { name, eras, summary, color } = CHARACTERS_DATA[character]
   return (
     <Term
       term={{
-        name,
+        name: name,
         short: `Présent ${
           typeof eras === "string"
             ? "à toutes les ERAS"
@@ -25,9 +27,13 @@ function Character({
         }`,
         summary,
         color,
+        regular: true,
       }}
-      link={`${PAGES_DATA[PAGES.L_UNIVERS_PERSONNAGES_ET_ENTITES].url}#${name}`}
-      className={`font-regular ${className || ""}`}
+      link={`${
+        PAGES_DATA[PAGES.L_UNIVERS_PERSONNAGES_ET_ENTITES].url
+      }#${name.replace(/ /g, "-")}`}
+      outline={outline}
+      className={className}
     />
   )
 }
