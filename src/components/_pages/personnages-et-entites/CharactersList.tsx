772a8e49519d8bdce9c3728963a4d08205d3dcea
Rimarok <romaric.ruga@gmail.com>
2022-05-06 08:12:22 +0200

    CharacterList tsx: use of particle

--- src/components/_pages/personnages-et-entites/CharactersList.tsx
+++ src/components/_pages/personnages-et-entites/CharactersList.tsx
@@ -1,15 +1,23 @@
 import * as React from "react"
-import { CHARACTERS } from "../../../../data/characters"
+import { CHARACTERS, CHARACTERS_DATA } from "../../../../data/characters"
 import Character from "../../Character"
 
 function CharactersList({ characters }: { characters: CHARACTERS[] }) {
   return (
     <ul className="mt-5">
-      {characters.map(character => (
-        <li key={character} className="mb-3">
-          <Character character={character as CHARACTERS} />
-        </li>
-      ))}
+      {characters.map(character => {
+        const { particle } = CHARACTERS_DATA[character]
+        return (
+          <li key={character} className="mb-3">
+            {particle ? (
+              <span className="text-capitalize">{particle}</span>
+            ) : (
+              ""
+            )}
+            <Character character={character as CHARACTERS} />
+          </li>
+        )
+      })}
     </ul>
   )
 }
