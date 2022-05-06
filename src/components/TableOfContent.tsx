8dc3e3546ea86c3a1c6b7c9aa1d94864d4886b27
Rimarok <romaric.ruga@gmail.com>
2022-05-06 08:10:13 +0200

    TableContent tsx: add className prop

--- src/components/TableOfContent.tsx
+++ src/components/TableOfContent.tsx
@@ -3,12 +3,14 @@ import React from "react"
 export function TableOfContent({
   sections,
   unstyled = true,
+  className,
 }: {
   sections: { [key: string]: string }
   unstyled?: boolean
+  className?: string
 }) {
   return (
-    <ul className={`${unstyled ? "list-unstyled" : ""}`}>
+    <ul className={`${unstyled ? "list-unstyled" : ""} ${className || ""}`}>
       {Object.entries(sections).map(([id, label]) => (
         <li key={id}>
           <a href={"#" + id}>{label}</a>
