bd7f5c5110604fcfc4a091f655e49eb88055fc38
Rimarok <romaric.ruga@gmail.com>
2022-05-06 08:09:57 +0200

    Era tsx: add outline & text color

--- src/components/Era.tsx
+++ src/components/Era.tsx
@@ -1,16 +1,19 @@
 import * as React from "react"
+import { COLORS } from "../../data/colors"
 import { ERAS, ERAS_DATA } from "../../data/eras"
 import { PAGES, PAGES_DATA } from "../../data/pages"
 import Term from "./Term"
 
 function Era({
   era,
-  className,
   outline,
+  textColor,
+  className,
 }: {
   era: ERAS
-  className?: string
   outline?: boolean
+  textColor?: COLORS
+  className?: string
 }) {
   const { name, index, summary, color } = ERAS_DATA[era]
   return (
@@ -23,6 +26,7 @@ function Era({
       }}
       link={`${PAGES_DATA[PAGES.L_UNIVERS_ERAS].url}#${name}`}
       outline={outline}
+      textColor={textColor}
       className={className}
     />
   )
