bdd2ccf535401ec4f229803da2427ca9ef11e7a8
Rimarok <romaric.ruga@gmail.com>
2022-05-11 06:27:47 +0200

    ImageRow: add gradient prop

--- src/components/_rows/ImageRowPositionAbsolute.tsx
+++ src/components/_rows/ImageRowPositionAbsolute.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactElement } from "react"
+import React, { CSSProperties } from "react"
 
 export function ImageRowPositionAbsolute({
   src,
@@ -6,18 +6,30 @@ export function ImageRowPositionAbsolute({
   imgStyle,
   className,
   imgClassName,
+  gradient,
 }: {
   src: string
   style?: CSSProperties
   className?: string
   imgStyle?: CSSProperties
   imgClassName?: string
+  gradient?: string
 }) {
   return (
     <div
       className={`position-absolute w-100 full-img-container align-items-center ${className}`}
       style={style}
     >
+      {gradient && (
+        <div
+          className="position-absolute w-100 h-100"
+          style={{
+            background: `linear-gradient(to bottom, #0000, ${gradient})`,
+          }}
+        >
+          {" "}
+        </div>
+      )}
       <img
         src={src}
         className={`w-100 cover ${imgClassName || ""}`}
