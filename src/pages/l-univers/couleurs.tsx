39e019fb593d9044740309cce8ca7fab92d55a3f
Rimarok <romaric.ruga@gmail.com>
2022-05-16 17:26:18 +0200

    page couleurs: rework to display all related words

--- src/pages/l-univers/couleurs.tsx
+++ src/pages/l-univers/couleurs.tsx
@@ -1,14 +1,79 @@
 import React from "react"
+import { CHARACTERS, CHARACTERS_DATA } from "../../../data/characters"
 import { COLORS, COLORS_DATA } from "../../../data/colors"
+import { ERAS, ERAS_DATA } from "../../../data/eras"
 import { PAGES } from "../../../data/pages"
+import { PLACES, PLACES_DATA } from "../../../data/places"
+import { TERMS, TERMS_DATA } from "../../../data/terms"
+import { TRIBES, TRIBES_DATA } from "../../../data/tribes"
+import { WordsDataSet } from "../../../data/_types/WordsDataSet"
+import Character from "../../components/Character"
+import Era from "../../components/Era"
 import { Header } from "../../components/Header"
 import { Layout } from "../../components/Layout/Layout"
+import Place from "../../components/Place"
+import Term from "../../components/Term"
+import Tribe from "../../components/Tribe"
 import { UniversePagesCTA } from "../../components/_ctas/UniversePagesCTA"
 import { SectionRow } from "../../components/_rows/SectionRow"
 import "../../styles/global.scss"
+import { WORD_TYPES } from "../../_enums/word-types.enum"
 
 const pageId = PAGES.L_UNIVERS_COULEURS
 const title = "Signification des couleurs"
+
+type ColorWordEntry = {
+  key: string
+  type: WORD_TYPES
+}
+
+type ColorWordEntriesSet = { [color in COLORS]?: ColorWordEntry[] }
+
+function addWordEntryToColor(
+  colorsWords: ColorWordEntriesSet,
+  color: COLORS,
+  wordEntry: ColorWordEntry
+) {
+  if (!colorsWords[color]) {
+    colorsWords[color] = []
+  }
+  colorsWords[color].push(wordEntry)
+  return colorsWords
+}
+
+const COLORS_WORD_ENTRIES: ColorWordEntriesSet = ([
+  {
+    type: WORD_TYPES.TERM,
+    dataSet: TERMS_DATA,
+  },
+  {
+    type: WORD_TYPES.CHARACTER,
+    dataSet: CHARACTERS_DATA,
+  },
+  {
+    type: WORD_TYPES.ERA,
+    dataSet: ERAS_DATA,
+  },
+  {
+    type: WORD_TYPES.PLACE,
+    dataSet: PLACES_DATA,
+  },
+  {
+    type: WORD_TYPES.TRIBE,
+    dataSet: TRIBES_DATA,
+  },
+] as { type: WORD_TYPES; dataSet: WordsDataSet }[]).reduce(
+  (colorsWords, { type, dataSet }) => {
+    Object.entries(dataSet).forEach(([key, data]) =>
+      addWordEntryToColor(colorsWords, data.color, {
+        key,
+        type,
+      })
+    )
+    return colorsWords
+  },
+  {}
+)
 
 export default function Couleurs() {
   return (
@@ -30,7 +95,8 @@ export default function Couleurs() {
           >
             <>
               <p>
-                Dans l'univers visuel de l'HIGHBS-BOK,{" "}
+                Dans l'univers visuel de l'
+                <Term term={TERMS.HIGHBS_BOK} />,{" "}
                 <b>
                   les couleurs tiennent une place aussi importante que les
                   termes du <a href="/l-univers/glossaire">glossaire</a>
@@ -54,28 +120,70 @@ export default function Couleurs() {
                       ![COLORS.LIGHT, COLORS.MUTED, COLORS.DARK].includes(color)
                   )
                   .map(color => {
-                    const {
-                      hexa: backgroundColor,
-                      description,
-                      colorWhite,
-                    } = COLORS_DATA[color]
+                    const { hexa, description, colorWhite } = COLORS_DATA[color]
                     return (
                       <tr key={color}>
                         <td>
                           <span
-                            className={`badge rounded-pill w-100 ${
+                            className={`badge rounded-pill text-capitalize w-100 ${
                               colorWhite ? " text-light" : ""
                             }`}
                             style={{
-                              backgroundColor,
+                              backgroundColor: hexa,
                               maxWidth: "25vw",
                               whiteSpace: "initial",
                             }}
                           >
-                            {backgroundColor} - <i>{color}</i>
+                            {color.toLowerCase().replace(/\_/g, " ")}
                           </span>
                         </td>
-                        <td>{description}</td>
+                        <td>
+                          {COLORS_WORD_ENTRIES[color].map(({ key, type }) => {
+                            const _key = `${type}-${key}`
+                            switch (type) {
+                              case WORD_TYPES.TERM:
+                                return (
+                                  <Term
+                                    key={_key}
+                                    term={key as TERMS}
+                                    className="me-2"
+                                  />
+                                )
+                              case WORD_TYPES.CHARACTER:
+                                return (
+                                  <Character
+                                    key={_key}
+                                    character={key as CHARACTERS}
+                                    className="me-2"
+                                  />
+                                )
+                              case WORD_TYPES.ERA:
+                                return (
+                                  <Era
+                                    key={_key}
+                                    era={key as ERAS}
+                                    className="me-2"
+                                  />
+                                )
+                              case WORD_TYPES.PLACE:
+                                return (
+                                  <Place
+                                    key={_key}
+                                    place={key as PLACES}
+                                    className="me-2"
+                                  />
+                                )
+                              case WORD_TYPES.TRIBE:
+                                return (
+                                  <Tribe
+                                    key={_key}
+                                    tribe={key as TRIBES}
+                                    className="me-2"
+                                  />
+                                )
+                            }
+                          })}
+                        </td>
                       </tr>
                     )
                   })}
