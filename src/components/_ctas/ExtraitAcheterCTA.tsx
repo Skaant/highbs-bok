8d0024e415c65baa01b359aff156a68688a6ffdb
Rimarok <romaric.ruga@gmail.com>
2022-05-17 15:12:33 +0200

    ExtraitAcheterCTA: add border prop

--- src/components/_ctas/ExtraitAcheterCTA.tsx
+++ src/components/_ctas/ExtraitAcheterCTA.tsx
@@ -1,14 +1,16 @@
 import React from "react"
-import { COLORS } from "../../../data/colors"
+import { COLORS, COLORS_DATA } from "../../../data/colors"
 import { EXTRACT_DOWNLOAD_LINK } from "../../config"
 import { ButtonsMenu } from "../ButtonsMenu"
 
 export function ExtraitAcheterCTA({
   primaryBtnColor = COLORS.PURPLE,
   secondaryBtnColor = COLORS.GOLD,
+  border,
 }: {
   primaryBtnColor?: COLORS
   secondaryBtnColor?: COLORS
+  border?: COLORS
 }) {
   return (
     <ButtonsMenu
@@ -18,11 +20,17 @@ export function ExtraitAcheterCTA({
           href: "/acheter",
           color: primaryBtnColor,
           label: "Acheter un livret",
+          border:
+            border === primaryBtnColor &&
+            (COLORS_DATA[border]?.textWhite ? COLORS.LIGHT : COLORS.DARK),
         },
         {
           href: EXTRACT_DOWNLOAD_LINK,
           color: secondaryBtnColor,
           label: "Téléchargez l'extrait",
+          border:
+            border === secondaryBtnColor &&
+            (COLORS_DATA[border]?.textWhite ? COLORS.LIGHT : COLORS.DARK),
         },
       ]}
     />
