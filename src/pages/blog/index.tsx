b22b105ce01d53eb07214195eee196cde4417df4
Rimarok <romaric.ruga@gmail.com>
2022-06-01 18:55:21 +0200

    pages/blog: use of PAGES_DATA & ARTICLES_ELEMENTS

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import articles, { ArticleData } from "../../../data/articles"
-import { PAGES } from "../../../data/pages"
+import ARTICLES_ELEMENTS from "../../../data/articles.components"
+import { PAGES, PAGES_DATA } from "../../../data/pages"
 import { TERMS } from "../../../data/terms"
 import { Header } from "../../components/Header"
 import { Layout } from "../../components/Layout/Layout"
@@ -8,20 +9,19 @@ import Term from "../../components/Term"
 import MiniRowAbsolute from "../../components/_rows/MiniRowAbsolute"
 import "../../styles/global.scss"
 
-const pageId = PAGES.BLOG
-const title = "Articles publiés"
+const page = PAGES.BLOG
 
 export default function () {
   return (
-    <Layout pageId={pageId}>
+    <Layout pageId={page}>
       <>
-        <Header title={title} />
+        <Header title={PAGES_DATA[page].title} />
         <div className="container-fluid px-0">
           <div className="row px-4">
             <div className="col-12 col-md-10 col-lg-8 col-xl-6">
-              <h1>{title}</h1>
+              <h1>{PAGES_DATA[page].title}</h1>
               <p className="mb-5">
-                Je vous ai déjà dit que l'univers de l'
+                Je vous ai déjà dit que l'univers de l'{" "}
                 <Term term={TERMS.HIGHBS_BOK} /> était <b>vivant</b> ? Tout
                 n'est pas écrit, loin s'en faut. Et même sur ce qui est écrit,
                 il y en aurait encore des <b>choses à dire</b> !
@@ -31,19 +31,34 @@ export default function () {
           <MiniRowAbsolute id="data-plant" justify="right" />
           <div className="row px-4 pt-4 bg-green">
             <div className="col-12 col-md-10 col-lg-8 col-xl-6">
-              <h2>Liste des articles</h2>
+              <h2
+                className="display-2 text-light"
+                style={{
+                  marginTop: "8rem",
+                  marginBottom: "6rem",
+                }}
+              >
+                Articles publiés
+              </h2>
               <ul className="list-unstyled">
                 {articles
                   .slice(0)
                   .reverse()
                   .map(
-                    ({ slug, title, description, date, tags }: ArticleData) => (
+                    ({
+                      index,
+                      slug,
+                      title,
+                      description,
+                      date,
+                      tags,
+                    }: ArticleData) => (
                       <li className="mb-3 card shadow">
                         <a
                           href={`/blog/${slug}`}
                           className="p-4 text-dark card-body text-decoration-none"
                         >
-                          <h3>{title}</h3>
+                          <h3>{ARTICLES_ELEMENTS[index]?.title || title}</h3>
                           {description}
                           <br />
                           <br />
